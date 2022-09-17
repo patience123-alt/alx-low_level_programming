c62244d6f6d66543014010f7f0dbc511c03d9694
Patience123-alt <uchbabynga2016@gmail.com>
2022-09-16 18:18:21 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+*Description: using the main function
+* this program prints "Programming is like building a multingual puzzle
+* Return; 0
+*/
+int main(void)
+{
+       printf("with proper grammar, but the outcome is a place  of art,\n");
+}

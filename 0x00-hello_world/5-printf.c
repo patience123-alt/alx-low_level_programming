5ebcf02e409ddaf1672bbe50c115b5604d7783f4
Patience123-alt <uchbabynga2016@gmail.com>
2022-09-18 13:22:10 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 *main-entry point for the program.
 *
@@ -7,6 +6,6 @@
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a place of art,\n");
+pinrtf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }

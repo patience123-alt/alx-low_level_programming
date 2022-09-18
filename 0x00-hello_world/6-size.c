7432ac5a761a24b0afbb87828d2e64e2b221f6ce
Patience123-alt <uchbabynga2016@gmail.com>
2022-09-18 09:07:03 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main -is the entry point for the program.
+*
+*Return:zero if no error, Non-zero value if errors.
+*/
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d
+float e;
+printf("Size of a char: %d byte(s)\n", sizeof(a));
+printf("Size of an int: %d byte(s)\n", sizeof(b));
+printf("Size of a long int: %d byte(s)\n", sizeof(c);
+printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+printf("Size of a float: %d byte(s)\n", sizeof(e));
+return(0);
+}

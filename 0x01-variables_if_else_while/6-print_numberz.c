64513a024244d81a44428605936522374213d13f
Patience123-alt <uchbabynga2016@gmail.com>
2022-09-23 03:22:10 -0700

    print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
+{ 
 	int n;
-	for (n = 48; n < 58; n++)
-	{	
-		putchar(n);
+
+	for (n = 48; n < 58; n++);
+	{
+	putchar(n);
 	}
 	putchar('\n');
 	return (0);
